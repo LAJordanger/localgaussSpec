2c6be0893b087e702d8f812f4f2c158adbc6cbfc
LAJordanger <lars.jordanger@gmail.com>
2017-06-30 15:45:04 +0200

    Added the first batch of functions, define the points to investigate.

---
+++ R/LG_select_points.R
@@ -0,0 +1,206 @@
+################################################################################
+
+#' Select points for the local Gaussian inspections.
+#'
+#' The purpose of this function is to create the
+#' "points to inspect"-argument needed for the local Gaussian
+#' investigation.  
+#'
+#' @param .P1 A bivariate vector, with values in the range (0,1), that
+#'     gives the first endpoint.  A single number will be accepted, in
+#'     which case it will be registered as a diagonal point.  The
+#'     values will be converted by \code{qnorm} to get points in the
+#'     plane.
+#'
+#' @param .P2 A bivariate vector, with values in the range (0,1), that
+#'     gives the second endpoint.  A single number will be accepted,
+#'     in which case it will be registered as a diagonal point.  The
+#'     values will be converted by \code{qnorm} to get points in the
+#'     plane.  Note that \code{.P2} can coincide with \code{.P1} in
+#'     which case the values given to '.shape' will be ignored.
+#'
+#' @param .shape A bivariate vector of integers, that describes the
+#'     shape of the resulting "grid".  Note that it is required that
+#'     the first component of \code{.shape} must be a positive integer
+#'     and the second a non-negative integer.  This argument will,
+#'     when \code{.P1} and \code{.P2} are different, either specify a
+#'     rectangular/quadratic with the points \code{.P1} and \code{.P2}
+#'     as opposite corners, or it will specify points upon a line with
+#'     \code{.P1} and \code{.P2} as endpoints if the second component
+#'     is negative.  A single positive integer will be accepted, and
+#'     it will be interpreted as if the missing number should be zero.
+#'     This argument will be ignored if \code{.P1} and \code{.P2}
+#'     coincide completely.  If \code{.P1} and \code{.P2} coincide at
+#'     one of the components, and a bivariate vector with to positive
+#'     integers is given to \code{.shape}, then shape will be adjusted
+#'     to produce a line instead in order to avoid redundant
+#'     computations.  The values given in \code{.P1} and \code{.P2}
+#'     must be contained in the grid, so the value \code{1} can only
+#'     be used in \code{.shape} when \code{.P1} and \code{.P2} have a
+#'     common component.  If the components differ, the value will be
+#'     corrected to \code{2}, which is the smallest integer that gives
+#'     a reasonable result.
+#'
+#' 
+#'
+#' @return An array containing points computed according to the rules
+#'     specified under the description of the shape-argument.  The
+#'     three arguments \code{.P1}, \code{.P2} and \code{.shape} will
+#'     be stored as attributes, together with three additional
+#'     attrubes \code{Shape}, \code{Horizontal} and \code{Vertical},
+#'     of wich the last two will specify the points at the corner of
+#'     the grid, whereas \code{Shape} will contain the value "point"
+#'     if \code{.P1} and \code{.P2} are equal, the value "rectangle"
+#'     if \code{.shape} contains two positive integers, or else it
+#'     will contain the value "line.".  The result will in addition
+#'     have the class-attribute "LG_points"
+#'
+#' @export
+
+LG_select_points <- function(.P1, .P2, .shape) {
+    ##  Sanity check '.P1' and '.P2'.
+    .valid_points <- local({
+        .range <- range(.P1, .P2)
+        .range_OK <- min(.range) > 0 & max(.range) < 1
+        if (.range_OK) {
+            max(length(.P1), length(.P2)) <= 2
+        } else
+            FALSE
+    })
+    if (! .valid_points)
+        error(.argument = c(".P1", ".P2"),
+              c("Invalid values, insert univariate/bivariate values in",
+              "the open interval (0,1)."))
+    kill(.valid_points)
+    ##  Adjust the points to bivariate if necessary.
+    if (length(.P1) == 1)
+        .P1[2] <- .P1[1]
+    if (length(.P2) == 1)
+        .P2[2] <- .P2[1]
+    ##  Sanity check '.shape', when required.
+    if (! identical(x = .P1, .P2)) {
+        .valid_shape <- local({
+            .length <- length(.shape)
+            .length_OK <- .length <= 2 & .length > 0
+            if (.length_OK) {
+                ##  Check that the content satifies the requirements.
+                if (all(as.integer(.shape) == .shape)) {
+                    all(.shape[1] > 0,
+                        if (.length > 1)
+                            .shape[2] >= 0)
+                } else
+                    FALSE
+            } else FALSE
+        })
+        if (! .valid_shape) 
+            error(.argument = ".shape",
+                  c("This must either be a bivariate vector whose",
+                    "first component is a univiariate positive integer",
+                    "and whose second component is a nonegative integer.",
+                    "A single positive integer will also be accepted."))
+        kill(.valid_shape)
+        ##  Adjust '.shape' to bivariate if necessary.
+        if (length(.shape) == 1)
+            .shape[2] <- 0
+        ##  Adjust '.shape' (when necessary) to produce a line if
+        ##  there is an overlap on one of the components (overlap on
+        ##  both is excluded in this branch of the code)
+        if (.shape[1] > 0 & .shape[2] > 0) {
+            if (.P1[1] == .P2[1])
+                .shape[2] <- 0
+            if (.P1[2] == .P2[2]) {
+                .shape[1] <- .shape[2]
+                .shape[2] <- 0
+            }
+        }
+        ##  Check that the value '1' is not used when the points have
+        ##  different components, and if so update it to '2'.
+        for (i in 1:2) 
+            if (.shape[i] ==1)
+                if (.P1[i] != .P2[i])
+                    .shape[i] <- 2
+    } else
+        .shape <- c(1, 0)
+###-------------------------------------------------------------------
+    ##  Create the collection of points to return, in two steps: Find
+    ##  arrays containing all the points of interest first, then ues
+    ##  'qnorm' to find the points in the plane.
+    .array_of_points <-
+        if  (identical(x = .P1, .P2)) {
+            .key1 <- .P1[1]
+            .key2 <- .P1[2]
+            ##  Return the point with dimensional attributes.
+            structure(
+                .Data = .P1,
+                .Dim = c(1, 2))
+        } else {
+            ##  A line or a rectangle, "starting" at the lower right,
+            ##  introdcue '.p1' and '.p2' to achieve this.
+            if (.P1[1] < .P2[1]) {
+                .p1 <- .P1
+                .p2 <- .P2
+            } else {
+                if (.P1[1] == .P2[1]) {
+                    if (.P1[2] < .P2[2]){
+                        .p1 <- .P1
+                        .p2 <- .P2
+                    } else {
+                        .p1 <- .P2
+                        .p2 <- .P1
+                    }
+                } else {
+                    .p1 <- .P2
+                    .p2 <- .P1
+                }
+            }
+            ##  Find the key-points of interest
+            .key1 <- seq(
+                from = .p1[1],
+                to = .p2[1],
+                length.out = .shape[1])
+            .key2 <- seq(
+                from = .p1[2],
+                to = .p2[2],
+                length.out =
+                    if (.shape[2] == 0) {
+                        .shape[1]
+                    } else
+                        .shape[2])
+            ##  Create the array based on shape.
+            if (.shape[2] == 0) {
+                cbind(.key1, .key2)
+            } else
+                local({
+                    .tmp <- expand.grid(first = .key1, second = .key2)
+                    cbind(.tmp$first, .tmp$second)
+                })
+        }
+    ##  Update the array by the help of 'qnorm', and add the required
+    ##  attributes
+    structure(
+        .Data = qnorm(.array_of_points),
+        .Dim = dim(.array_of_points),
+        .Dimnames = list(
+            level = apply(X = .array_of_points,
+                          MARGIN = 1,
+                          FUN = paste,
+                          collapse = "_"),
+            comp = c("v1", "v2")),
+        class = c(LG_default$class$points,
+                  LG_default$class$array),
+        .P1 = .P1,
+        .P2 = .P2,
+        .shape = .shape,
+        Horizontal = qnorm(.key1),
+        Vertical = qnorm(.key2),
+        Shape =
+            if (identical(x = .P1, .P2)) {
+                "points"
+            } else {
+                if (.shape[2] == 0) {
+                    "line"
+                } else
+                    "rectangle"
+            }
+    )
+}
--- R/localgaussSpec-package.r
+++ R/localgaussSpec-package.r
@@ -1,5 +1,130 @@
-#' localgaussSpec.
+#' localgaussSpec
 #'
-#' @name localgaussSpec
+#' A package for the investigation of time series by means of
+#' Local Gaussian Spectral Densities.
+#'
+#' The localgaussSpec package contains the functions needed for the
+#' investigation by means of local Gaussian approximations.  Moreover,
+#' it also contains copies of the scripts needed for a reproduction of
+#' results mentioned in ...  See <helper-function> for details.
+#'
+#' @section Simulating data:
+#' Some stuff about the functions used for simulations, and the
+#' format of the result.
+#'
+#' @section Computation of local Gaussian approximations:
+#' blah blah...
+#'
+#' @section Computation of local Gaussian spectra:
+#' blah blah...
+#'
+#' @section Computation of bootstrap-based confidence intervals:
+#' blah blah...
+#' 
+###############
+## Add details here with regard to packages/functions to import.
+## #' @import foreach
+## #' @import shiny
+## #' @import stringr
+## #' @importFrom abind abind adrop
+## #' @importFrom alabama auglag
+## #' @importFrom ggplot2 ggplot coord_cartesian geom_hline geom_vline
+## #'     labs geom_line aes annotate theme geom_ribbon geom_boxplot
+## #'     cut_width
+## #' @importFrom graphics hist plot
+## #' @importFrom localgauss localgauss
+## #' @importFrom logspline logspline dlogspline plogspline
+## #' @importFrom mvtnorm dmvnorm pmvnorm
+## #' @importFrom magrittr and or divide_by
+## #' @importFrom markdown renderMarkdown
+## #' @importFrom methods findMethods is
+## #' @importFrom plyr aaply
+## #' @importFrom pryr where dots
+## #' @importFrom stats dnorm pnorm qnorm rnorm acf as.ts coef kernel lag
+## #'     mad median optim optimise optimize pacf quantile runif sd
+## #'     spec.pgram var
+## #' @importFrom timeDate kurtosis skewness
+## #' @importFrom tseries tsbootstrap
+## #' @importFrom ToolBox spy create_call restrict_array
+## #'     append_dimensions split_vector my_apply my_abind aa_restrict
+## #'     multiply_arrays add_arrays append_dimensions
+## #'     compare_spy_reports update_formals dimnames_union
+## #'     dimnames_intersect update_array nested_if skeleton_list
+## #'     aa_where_and_who my_aaply file_created array_from_files
+## #'     set_seed error kill this_function list_array_join
+## #'     list_array_dims array_to_list_array
+## #' @importFrom utils capture.output combn head object.size str tail
+#' @importFrom leanRcoding error kill
+#' @importFrom stats qnorm
+
+###############
+
 #' @docType package
+#' @name localgaussSpec
 NULL
+
+
+################################################################################
+#####  A list of the available (reproducible) time series, that will
+#####  be populated during the compilation of the package.
+#'
+#' Time Series Families
+#'
+#' The function that computes the samples will use keywords to select
+#' a relevant part from the list \code{TS_families}.  The idea is that
+#' this should help document all the parameters used in the sampling
+#' procedure, such that proper calls can be stored that later on can
+#' be resurrected if a reproduction of the computation is desired.
+## #'
+## #' To ensure documentation of the parameters used when
+## #' sampling, and to The idea with this list is tasks later on should
+## #' be simplified, in particular with regard to numerical
+## #' investigations where we would like to re-sample some time series a
+## #' lot of times with assorted values for its parameters.
+## #'
+## #' This is a list of lists containing four mandatory parts, and an
+## #' additional field to be used for cases where a method must be
+## #' specified.
+## #'
+## #'
+## #'
+## #' 1) The name of the package that contains the desired generating function.
+## #' 2) The name of the desired function.
+## #' 3) A list with the default values for its arguments.
+## #' 4) The name of the argument that decides the number of samples.
+## #' 5) Method needed to extract the correct part from some other package.
+#'
+#' @format A list of lists, the names at the top-level will be used as
+#'     "keys" that selects the relevant sub-list.  The sublists
+#'     contanins the parts \code{fun}, \code{package}, \code{args},
+#'     \code{size_name} and \code{method}, as described below:
+#'
+#' \describe{
+#' 
+#' \item{fun}{The name of the desired function, as a
+#'     character-string.}
+#' 
+#' \item{package}{The name of the package that contains \code{fun}, as
+#'     a character-string.}
+#'
+#' \item{args}{A list containing the default arguments to be used in
+#'     \code{fun}.  Reminder: In order for the package to compile, it
+#'     might be necessary to quote the list if there are references to
+#'     packages not imported into this package.}
+#'
+#' \item{size_name}{The name of the argument of \code{fun} that
+#'     specifies the number of samples to be created.}
+#'
+#' \item{method}{A character string that has to be specified if
+#'     \code{fun} needs to be "extracted" from some deeper level of
+#'     \code{package}.  Simply drop this part when it is superfluous.}
+#' 
+#' }
+#'
+## #' @usage This list is used as a reference for other functions.
+#' @name TS_families
+#' @keywords internal
+
+TS_families <- list()
+###  REMINDER: This list will be filled up when the files starting
+###  with "TS_family_" are read during the loading of the package.
