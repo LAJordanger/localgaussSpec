a9184a5cf29191dc6504138d179ed712cade53b4
LAJordanger <lars.jordanger@gmail.com>
2020-07-16 16:08:31 +0200

    Updated documentation for 'LG_plot_helper', and moved the option for the extraction of the underlying data to an internal function.

--- R/LG_plot_helper.R
+++ R/LG_plot_helper.R
@@ -1,72 +1,63 @@
 #' Helper-function for \code{LG_plot}.
 #'
-#' This is a helper function for \code{LG_plot}, that first extracts
-#' the desired array from file and then creates the plot of interest.
-#' The function are used as an helper in \code{LG_shiny}, in which
-#' case it also will take care of the restrictions in a manner that
-#' attempts to avoid the major restrictions when possible.  The
-#' interactive setting can trigger the creation of code needed in
-#' order to re-create a plot in a non-reactive setting too (e.g. as an
-#' example in a paper/presentation), in which case some of the
-#' internal workings of the function will be adjusted to allow for the
-#' function call to work without the \code{shiny}-package being
-#' loaded.
+#' @description This function extracts the desired array of local
+#'     Gaussian (auto- or cross-) correlations from file, and then it
+#'     sends those to the relevant plot-function in order to create
+#'     the plot of interest.
 #'
-#' NOTE: This function is exported in order for interesting plots to
-#' be recreated outside of the shiny-application, in which case the
-#' returned plot-objects also will contain attributes needed for the
-#' proper identification of the content of the plots.  The idea is
-#' that the required parameters of this function will be filled in by
-#' the shiny-application, and even though it is possible to adjust the
-#' input parameters manually, it is not recommended.
+#' @details This function is used as a helper in in the interactive
+#'     setup of \code{LG_shiny}, but it can also be called in a
+#'     non-reactive environment.  The interactive interface can be
+#'     used to trigger the creation of the code needed in order to use
+#'     this function in a non-reactive setting (which can be of
+#'     interest e.g. when an example should be used in a
+#'     paper/presentation).
+#'
+#' @details The plots created by this function when it is used in a
+#'     non-reactive setting, will contain an attribute \code{details}
+#'     that can be used to extract information about the content.  The
+#'     function \code{LG_extract_details} can be used to obtain the
+#'     same information that was available in the interactive
+#'     interface, or the user can extract the components of interest
+#'     in order to produce other presentations too.
 #'
 #' @param main_dir The part of the \code{data_dir}-argument from the
-#'     calling function \code{LG_shiny}, that specifies the main path
-#'     to where the information is stored.  Note that this argument
-#'     only is required when the function is called outside of the
-#'     shiny application.  (Inside of the shiny-application, the
-#'     relevant information will be extracted from \code{.env}.)
+#'     function \code{LG_shiny}, that specifies the main path to where
+#'     the information is stored.  Note that this argument only is
+#'     required when the function is called outside of the shiny
+#'     application.  (Inside of the shiny-application, the relevant
+#'     information will be extracted from \code{.env}.)
 #'
 #' @param input A list with the arguments needed in order to extract
 #'     the desired data for the plot of interest.
 #'
 #' @param input_curlicues A list that can be used when this function
 #'     is used outside of the \code{shiny}-application.  This can be
-#'     used to fine tune details related to the annotated information.
+#'     used to fine-tune details related to the annotated information.
 #'     It is also possible to drop annotation from the plot, which
 #'     might be of interest when many plots are to be included in a
 #'     grid-based setup.  The default value \code{NULL} will ensure
-#'     that the plot will be created with the same parameters used in
-#'     the \code{shiny}-application.
+#'     that the plot will be created with the same setting that was
+#'     used in the \code{shiny}-application (this might however not
+#'     produce decent-looking plots when a grid of plots is created
+#'     and saved to file).
 #'
 #' @param .env An environment in which \code{.arr} will be updated.
 #'     This argument can be skipped when called in a non-interactive
 #'     setting.
 #'
-#' @param .extract_LG_data_only Logical argument, default
-#'     \code{FALSE}.  The default value ensures that a plot is
-#'     created, whereas the value \code{TRUE} is used when the desired
-#'     outcome is an object containing the estimated local Gaussian
-#'     values.
-#' 
 #' @return The default behaviour is that a plot-object will be
 #'     returned, with attributes describing the content. The
 #'     information can be extracted from the plot-object by the help
-#'     of \code{LG_explain_plot}.  When \code{.extract_LG_data_only}
-#'     is \code{TRUE}, the result will be an object containing the
-#'     estimated local Gaussian values.
+#'     of \code{LG_explain_plot}.
 #' 
 #' @export
-
-
 
 LG_plot_helper <- function(
     main_dir,
     input,
     input_curlicues = NULL,
-    .env,
-    .extract_LG_data_only = FALSE) {
-###-------------------------------------------------------------------
+    .env) {
     ##  A minor adjustment to deal with the case when this function is
     ##  called outside of the interactive shiny-setup.
     if (missing(.env)) {
@@ -87,11 +78,10 @@ LG_plot_helper <- function(
         .AB_env <- LG_shiny_interface_1_helper(
             .env = .env,
             .approx = input$Approx)
-        ##  REMINDER, 2019-08-23: The result of the above function
-        ##  turned out to be a list. It is used other places to, so I
-        ##  guess I for the time being will use this approach as a
-        ##  workaround. Perhaps add an additional argument to deal
-        ##  with this case?
+        ##  REMINDER: The result of the above function turned out to
+        ##  be a list. It is used other places too, so for the time
+        ##  being I will use this approach as a workaround. Perhaps
+        ##  add an additional argument to deal with this case?
         if (is.list(.AB_env))
             .AB_env <- .AB_env[[1]]
         ##  Add a logical value to reveal that the function is called
@@ -105,12 +95,10 @@ LG_plot_helper <- function(
     ##  Add information about the curlicues that might have been given
     ##  by the user in the non-interactive case.
     input$input_curlicues <- input_curlicues
-###-------------------------------------------------------------------
     ##  Do nothing if no graphical component has been selected.
     if (is.na(input$TCS_type))
         return()
-###-------------------------------------------------------------------
-    ##  If the `get_code` check-box has been marked, return code that
+    ##  If the 'get_code' check-box has been marked, return code that
     ##  can be used to create the plot in a paper.
     if  (input$get_code) {
         ##  Create the code required for this function to work outside
@@ -119,9 +107,9 @@ LG_plot_helper <- function(
         ##  'input') and the functions of interest (i.e.
         ##  'LG_plot_helper' and 'LG_explain_plot').  The
         ##  'digest'-function is applied in order to ensure that
-        ##  unique names are used.
-        ##  Reminder, 2019-08-26: The selection below is not optimal.
-        .controls <- c("TCS_type", "window", "Boot_Approx", "TS_key",
+        ##  unique names are used.  REMINDER: This part must be
+        ##  updated!
+        .controls <- c("TCS_type", "window", "Boot_Approx", ## "TS_key",
                        "confidence_interval", "levels_Diagonal",
                        "bw_points", "cut", "frequency_range", "type",
                        "levels_Horizontal", "TS", "TS_lags", "S_type",
@@ -150,22 +138,18 @@ LG_plot_helper <- function(
           "",
           deparse(LG_explain_plot_quote)))
     }
-###-------------------------------------------------------------------
     ##  The case of interest for the second iteration of this function
     ##  is the part concerning 'TS', in which case we need to pick out
     ##  the desired time series from the present 'info'-object.
     if (input$TCS_type == "T") {
         return()
     }
-###-------------------------------------------------------------------
     ##  The function 'LG_plot_load' takes care of the loading and
     ##  computations required for the desired plots to be computed, we
     ##  need to extract the desired 'look_up'-information first.
-###-------------------------------------------------------------------
-    ##  Use 'LG_lookup' to create the lookup-information.
     look_up <- LG_lookup(input = input,
                          .AB_env = .AB_env)
     LG_plot_load(look_up = look_up,
                  .env = .env,
-                 .extract_LG_data_only = .extract_LG_data_only)
+                 .extract_LG_data_only = FALSE)
 }
