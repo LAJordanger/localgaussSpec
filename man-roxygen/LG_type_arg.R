9cdffceaa1619940ae4ab160437bbc012d558442
LAJordanger <lars.jordanger@gmail.com>
2020-07-17 23:52:04 +0200

    Added a note regarding the authors disapproval of 'LG_type' being "par_one".

--- man-roxygen/LG_type_arg.R
+++ man-roxygen/LG_type_arg.R
@@ -2,3 +2,14 @@
 #'     Local Gaussian Autocorrelations be based on the approach using
 #'     five parameters or the simplified approach using only one
 #'     parameter.  The default choice is \code{"par_five"}.
+
+#' @note Regarding the case where the \code{LG_type}-argument is equal
+#'     to "par_one": The author of this package has always considered
+#'     the "par_one"-approach to be reasonable when the aim of the
+#'     investigation is to estimate a density at a given point.
+#'     However, the extraction of the correlation value from the
+#'     resulting density-estimate will in general not capture the
+#'     local geometrical properties of the targeted distribution at
+#'     the point of investigation.  The "par_one"-approach is as such
+#'     (in general) a complete waste of computation resources.
+#'
