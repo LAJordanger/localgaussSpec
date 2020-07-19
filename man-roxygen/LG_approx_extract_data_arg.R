4f5a55956f40fffa3fd08f3c51f753819451b4d5
LAJordanger <lars.jordanger@gmail.com>
2020-07-19 12:24:09 +0200

    Deleted obsolete functions.

--- man-roxygen/LG_approx_extract_data_arg.R
+++
@@ -1,7 +0,0 @@
-#' @param LG_approx_extract_data The list created when
-#'     \code{LG_approx_extract} is used upon the Local Gaussian
-#'     Approximations, i.e. the restricted subset of data needed for
-#'     the computation of the local Gaussian spectra.  The list does
-#'     also specify what kind of Local Gaussian Spectral Densities
-#'     that should be created i.e. if it should be based on those from
-#'     one ore five parameters.
