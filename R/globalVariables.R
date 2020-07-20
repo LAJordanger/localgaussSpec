e6de6da30c7ea0107554c754cad25c0546ec27e8
LAJordanger <lars.jordanger@gmail.com>
2020-07-20 09:19:13 +0200

    Updated the declaration of the global variables.

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,50 +1,23 @@
-##  This file was created Mon Feb 15 13:07:31 2016, by
-##  'ToolBoox::find_no_visible_global_variables'.  Do not edit by hand.
+##  Declaration of global variables.
 
-my_globalVariables <- 
-    c("adjustment",
-      ## ".b1_2",
-      ## ".b2_2",
-      ## "backup_show_graphical",
-      "block_median",
-      "boot_type",
-      "collected_bookmark",
-      ".details",
-      "dir_bookmark",
-      "done_before",
-      "folder_defaults",
-      "graph_changes",
-      "info",
-      "info_path",
-      "ingoing_list_array",
-      ".input",
-      ## "interface_or_plot",
-      ## ".log2pi",
-      ## "m.0.5.xx",
-      ## "m.0.5.xx_yy",
-      ## "m.0.5.yy",
-      ## "m1",
-      ## "m.1",
-      ## "m.x",
-      ## "m.xy",
-      ## "m.y",
-      ## "name_of_new_dimension",
-      ## "new_dimnames",
-      ## ".optimal_rho",
-      "omega",
-      "path_changes",
-      ## ".path_selection_list",
-      ## "pi.2",
-      "save_dir",
-      "spectra_CI",
-      "sub_piece",
-      ".this_function", ##  Check the code!!!
-      "TS_content",
-      "value"
-      ## "update_new_dimnames_quote",
-      ## "x0",
-      ## "y0"
-      )
+my_globalVariables <- c(
+    "folder_defaults", # LG_bookkeeping
+    "dir_bookmark", # LG_bookkeeping
+    "info", # LG_bookkeeping
+    "info_path", # LG_bookkeeping
+    "value", # LG_create_plot_df
+    "omega", # LG_create_plot_df
+    "orig", # LG_create_plot_df
+    ".tmp", # LG_lookup_details_NC_fail
+    "x", # LG_plot_complex
+    "y", # LG_plot_complex
+    "bw_points", # LG_plot_heatmap
+    "value", # LG_plot_heatmap
+    "omega", # LG_plot_heatmap
+    "m", # LG_plot_heatmap
+    "info", # LG_sanity_checks
+    "save_dir" # TS_acr
+)
 
 utils::globalVariables(my_globalVariables)
 rm(my_globalVariables)
