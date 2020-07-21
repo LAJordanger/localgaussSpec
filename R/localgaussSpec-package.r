5245236533692c1edde600b5c79643f3cb8362e9
LAJordanger <lars.jordanger@gmail.com>
2020-07-21 08:24:50 +0200

    Updated documentation for 'localgaussSpec-package'.

--- R/localgaussSpec-package.r
+++ R/localgaussSpec-package.r
@@ -1,27 +1,85 @@
 #' localgaussSpec
 #'
-#' A package for the investigation of time series by means of
-#' Local Gaussian Spectral Densities.
+#' @description A package for the investigation of univariate and
+#'     multivariate time series by means of Local Gaussian (auto- and
+#'     cross-) correlations, and the corresponding spectral densities.
 #'
-#' The localgaussSpec package contains the functions needed for the
-#' investigation by means of local Gaussian approximations.  Moreover,
-#' it also contains copies of the scripts needed for a reproduction of
-#' results mentioned in ...  See <helper-function> for details.
+#' @section Overview:
 #'
-#' @section Simulating data:
-#' Some stuff about the functions used for simulations, and the
-#' format of the result.
-#'
-#' @section Computation of local Gaussian approximations:
-#' blah blah...
-#'
-#' @section Computation of local Gaussian spectra:
-#' blah blah...
-#'
-#' @section Computation of bootstrap-based confidence intervals:
-#' blah blah...
+#' The \code{localgaussSpec}-package investigates strictly stationary
+#' time series by the help of a local Gaussian approach.  This implies
+#' that local Gaussian auto- and cross-correlations are computed for a
+#' selection of lags and a selection of points -- and based on this it
+#' is then possible to investigate the corresponding (m-truncated)
+#' local Gaussian spectra.  The local Gaussian correlation coincides
+#' with the ordinary correlation when a Gaussian structure is
+#' investigated, which implies that it is of interest to compare the
+#' ordinary spectra and the local Gaussian spectra since that can
+#' reveal the presence of non-Gaussian dependency structures in the
+#' time series under investigation.
 #' 
-###############
+#' @section The scripts for reproducibility of results:
+#'
+#' The scripts that are included in this package provide a simple way
+#' to see how the different key-functions should be put together in
+#' order to investigate both simulated and real examples.  These
+#' scripts are included in order to allow interested readers to
+#' reproduce the results and figures in the papers based on this local
+#' Gaussian approach, but they can easily be modified in order to
+#' investigate similar investigations for other cases.
+#'
+#' @section Workflow for simulated time series:
+#'
+#' The scripts for the simulated time series are based on the
+#' following sequence of steps and key functions.  The
+#' \code{localgaussSpec} package must be loaded, and a
+#' \code{main_dir}-argument must be specified.  The function
+#' \code{TS_sample} is then called in order to create the desired
+#' collection of samples, and this is done by the help of a
+#' \code{TS_key}-argument and parameters suitable for this particular
+#' key. The required generating functions is part of an internal list
+#' called \code{TS_family}, and this list can be extended on demand.
+#' Based on the generated time series, a unique \code{save_dir} is
+#' computed, and the function \code{TS_LG_object} is then used to
+#' initiate a file-hierarchy, in which the results of the local
+#' Gaussian investigation will be stored.  The next step is then to
+#' specify some details related to the tuning parameters for the
+#' estimation algorithm, and for this part the function
+#' \code{LG_select_points} can be used to specify different
+#' configurations of the points to be investigated (e.g. points along
+#' the diagonal or points in a rectangular grid).  After this, the
+#' scribe-function \code{LG_approx_scribe} is called in order to
+#' perform assorted tasks related to the computation, including a
+#' simple check that can prevent previously computed tasks to be
+#' recomputed.  If a new computation is encountered, then the
+#' scribe-function will save the resulting data into the file
+#' hierarchy. The scribe will return a \code{main_dir}-argument and a
+#' \code{data_dir}-argument, which is needed in order for
+#' \code{LG_shiny} to start a \code{shiny}-application that enables an
+#' interactive inspection of the resulting local Gaussian auto- and
+#' cross-correlations, together with different graphical
+#' visualisations of the corresponding local Gaussian spectra.
+#'
+#' @section Workflow for real data:
+#'
+#' The scripts for the real samples are quite similar to those
+#' described for the simulated time series.  An obvious difference is
+#' that \code{TS_sample} should not be used, since a sample after all
+#' is present.  This real sample is sent into \code{TS_LG_object},
+#' which creates a unique \code{save_dir} based on the given
+#' observations.  After this the same procedure is applied with regard
+#' to selecting the tuning parameters for the local Gaussian
+#' estimation, and \code{LG_approx_scribe} is used on the single
+#' sample.  After this, the function \code{LG_boot_approx_scribe} can
+#' be used to produce resampled versions of the time series, and based
+#' on the local Gaussian estimates from these resampled versions it is
+#' then possible to obtain pointwise confidence intervals for the
+#' estimates based on the original sample.  This scribe-function will
+#' also return a \code{main_dir}-argument and a
+#' \code{data_dir}-argument, in order for the \code{LG_shiny}-function
+#' to start the interactive \code{shiny}-investigation.
+#' 
+
 ## Add details here with regard to packages/functions to import.
 #' @import shiny
 #' @importFrom abind abind adrop
@@ -54,81 +112,7 @@
 #' @importFrom tools toTitleCase
 #' @importFrom utils capture.output combn head object.size str tail
 
-
-
-###############
-
 #' @docType package
 #' @name localgaussSpec
+
 NULL
-
-
-################################################################################
-#####  A list of the available (reproducible) time series, that will
-#####  be populated during the compilation of the package.
-#'
-#' Time Series Families
-#'
-#' The function that computes the samples will use keywords to select
-#' a relevant part from the list \code{TS_families}.  The idea is that
-#' this should help document all the parameters used in the sampling
-#' procedure, such that proper calls can be stored that later on can
-#' be resurrected if a reproduction of the computation is desired.
-## #'
-## #' To ensure documentation of the parameters used when
-## #' sampling, and to The idea with this list is tasks later on should
-## #' be simplified, in particular with regard to numerical
-## #' investigations where we would like to re-sample some time series a
-## #' lot of times with assorted values for its parameters.
-## #'
-## #' This is a list of lists containing four mandatory parts, and an
-## #' additional field to be used for cases where a method must be
-## #' specified.
-## #'
-## #'
-## #'
-## #' 1) The name of the package that contains the desired generating function.
-## #' 2) The name of the desired function.
-## #' 3) A list with the default values for its arguments.
-## #' 4) The name of the argument that decides the number of samples.
-## #' 5) Method needed to extract the correct part from some other package.
-#'
-#' @format A list of lists, the names at the top-level will be used as
-#'     "keys" that selects the relevant sub-list.  The sublists
-#'     contanins the parts \code{fun}, \code{package}, \code{args},
-#'     \code{size_name} and \code{method}, as described below:
-#'
-#' \describe{
-#' 
-#' \item{fun}{The name of the desired function, as a
-#'     character-string.}
-#' 
-#' \item{package}{The name of the package that contains \code{fun}, as
-#'     a character-string.}
-#'
-#' \item{args}{A list containing the default arguments to be used in
-#'     \code{fun}.  Reminder: In order for the package to compile, it
-#'     might be necessary to quote the list if there are references to
-#'     packages not imported into this package.}
-#'
-#' \item{size_name}{The name of the argument of \code{fun} that
-#'     specifies the number of samples to be created.}
-#'
-#' \item{method}{A character string that has to be specified if
-#'     \code{fun} needs to be "extracted" from some deeper level of
-#'     \code{package}.  Simply drop this part when it is superfluous.}
-#' 
-#' }
-#'
-## #' @usage This list is used as a reference for other functions.
-#' @name TS_families
-#' @keywords internal
-
-TS_families <- if (exists("TS_families", inherits = FALSE)) {
-                   TS_families
-               } else
-                  list()
-
-
-###  REMINDER: This list will be created when the files starting with
-###  "TS_family_" are read during the loading of the package.
