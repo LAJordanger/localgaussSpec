026869b29c48959af5df24d25455f4a4c1f5e4c9
LAJordanger <lars.jordanger@gmail.com>
2020-09-22 09:57:00 +0200

    Script updated due to new functionality of 'LG_plot_distance'.

--- inst/scripts/U/P1_fig_D5/4_Plot.R
+++ inst/scripts/U/P1_fig_D5/4_Plot.R
@@ -31,14 +31,6 @@
 
 ###----------------------------------------------------------------###
 
-##  WARNING: The distance-part of this script contains solutions based
-##  on internal code from the 'localgaussSpec'-package.  This implies
-##  that it almost certainly will have to be updated at some point.
-##  The plan is to implement a proper function for this task, and the
-##  mess in this script will then be replaced with proper code.
-
-#####------------------------------------------------------------#####
-
 ##  Specify the packages required for this script.
 
 library(localgaussSpec)
@@ -83,18 +75,17 @@ input_common <- list(
     global_local = "local",
     spectra_f_or_F = "f",
     drop_annotation = TRUE)
+rm(..Approx)
 
 ##  Initate the lists used to store the different plots, and the
 ##  annotations that is needed.  Note that the annotation is dropped
 ##  in the initially created plots, since it might be necessary to
 ##  adjust the size when they are included in a grid-based setting.
 
-
 .names <- c("lower", "center", "upper")
 heatmap_plots <- list()
 heatmap_annotations <- list()
 distance_plots <- list()
-distance_annotations <- list()
 
 .levels <- 1:3
 .levels_Diagonal <- 1
@@ -130,172 +121,31 @@ for (.levels_Diagonal in .levels) {
     heatmap_annotations[[.name]] <- attributes(
         x = heatmap_plots[[.name]])$curlicues$text
     
-    ##  The solution for the distance-based plots is a bit messy,
-    ##  since it is based on code extracted from other functions.
-    ##  This will hopefully be resolved in a future update.
+    ##  The next part deals with the distance-based plot.  Note: The
+    ##  present incarnation of the code primarily aims at removing
+    ##  internal functions from the scripts, and it is thus not
+    ##  possible to tweak the annotations of this plot in the same
+    ##  manner used for the heatmap-plot.
 
-    .extracted <- localgaussSpec:::LG_plot_helper_extract_data_only(
-            main_dir = ..main_dir,
-            input =
-                c(input_common,
-                  list(L2_distance_normal = TRUE,
-                       L2_inspection_vbmL = "b",
+    distance_plots[[.name]] <- LG_plot_helper(
+        main_dir = ..main_dir,
+        input = c(input_common,
+                  list(distance_plot = TRUE,
+                       distance_plot_b_v_m_L = "b",
                        levels_Diagonal = .levels_Diagonal)),
-            input_curlicues = list(
-                x.label_low_high = c(0, 1.5),
-                b_value = list(include = FALSE),
-                NC_value = list(short_or_long_label = "short")))
+        input_curlicues = list(
+            NC_value = list(short_or_long_label = "short"),
+            distance_plot = list(
+                add_points_at_bws = "0.5",
+                size = .7,
+                shape = 1,
+            colour = "blue")))  +
+        theme(axis.ticks = element_line(size = 0.3),
+              axis.ticks.length = unit(.06, "cm"),
+              axis.text = element_text(size = 6))
 
-    distance_annotations[[.name]] <-
-        .extracted$look_up$curlicues$text
-
-    ##  Adjust the extracted environment so the internal function
-    ##  'LG_spectrum_norm' can digest it.
-
-    .env <- .extracted$..env
-    .env$look_up <- .extracted$look_up
-    look_up <- .env$look_up
-    rm(.extracted)
-
-    ##  Use the norm-function to compute the different norms and
-    ##  distances.
-    
-    ## .L2_norm <- localgaussSpec:::LG_spectrum_norm(
-    ##                                  C1_env = .env,
-    ##                                  W1 = localgaussSpec:::myWindows[[look_up$window]](.cut = look_up$cut))
-
-    ## ##  REMINDER: This is not used yet in the script below, it is
-    ## ##  included here as a placeholder for the time when it is due to
-    ## ##  adjust this mess.
-    
-    #####  Attempting at geting stuff done in the same way as for the
-    #####  levels/points versus the L2-norm.
-
-    cache <- look_up$cache
-
-    ##  For bandwidth-versus-norm: The '.env'-targets of interest are
-    ##  contained in ".LGC_array_local" and cache$weights_f.  The structure
-    ##  of 'weights_f' imply that the restriction of the data should be
-    ##  taken with regard to the selected m-truncation.  Reminder: The
-    ##  names in the '.weight'-list refer to the 'cut'-value, which is
-    ##  one higher than the 'm'-value.
-    ###############
-    ##  Need the global data too, in order for the plot to contain a
-    ##  horisontal line that shows the norm of the global spectrum.
-    ###############
-
-    ##  First: Extract the correlations of interest, with a
-    ##  restriction to one of the points of interest.
-    
-    ..lag_values <- names(.env[[cache$weights_f]][[as.character(look_up$cut)]])
-    ..restrict <- list(variable = "rho",
-                       levels = look_up$levels_point,
-                       lag = ..lag_values)
-    .data <- leanRcoding::restrict_array(
-        .arr = .env[["LGC_array_local"]]$on_diag,
-        .restrict = ..restrict,
-        .drop = TRUE,
-        .never_drop = c("lag", "bw_points"))
-    .global_data <- leanRcoding::restrict_array(
-        .arr = .env[["LGC_array_global"]],
-        .restrict = list(TS = "TS_for_analysis",
-                         lag = ..lag_values),
-        .drop = TRUE,
-        .never_drop = c("lag", "content"))
-    rm(..lag_values, ..restrict)
-
-    ## Next: Compute the product of the correlations with the weights.
-
-    .weighted_data <- leanRcoding::multiply_arrays(
-        .arr1 = .data,
-        .arr2 = .env[[cache$weights_f]][[as.character(look_up$cut)]])
-    .global_weighted_data <- leanRcoding::multiply_arrays(
-        .arr1 = .global_data,
-        .arr2 = .env[[cache$weights_f]][[as.character(look_up$cut)]])
-
-    ##  Next: Compute the norms.  In this "on_diagonal"-case for local
-    ##  Gaussian autocorrelations, the squared norms are obtained by
-    ##  squaring the '.weighted_data', summing over the lags,
-    ##  multiplying with 2 (due to folding) and add 1 (to include the
-    ##  lag-zero-term).  Take the square root to get the norms.
-
-    .the_norms <- sqrt(
-        1 + 2 * leanRcoding::my_apply(
-                    X = .weighted_data^2,
-                    MARGIN = "bw_points",
-                    FUN = sum))
-    .the_global_norm <- sqrt(
-        1 + 2 * leanRcoding::my_apply(
-                    X = .global_weighted_data^2,
-                    MARGIN = "content",
-                    FUN = sum))
-
-    ##  Create the data-frame needed for the plot.
-
-    .df <- reshape2::melt(data = .the_norms)
-
-    ##  Specify stuff to be included in the plot.
-    .plot_title <- sprintf("Bandwidth vs. norm for the m=%s local Gaussian autospectrum",
-                           look_up$m_selected)
-    .aes_mapping <- aes(x = bw_points,
-                        y = value)
-    .xlab_expression <- "b"
-    .ylab_expression <- ""
-
-    library(ggplot2)
-
-    ##  Create the plot.
-    .plot <- ggplot(data=.df,
-                    mapping = .aes_mapping) +
-        geom_line(size = 0.1,
-                  colour = "brown") +    
-        ##  Adjust legend and labels.
-        labs(fill = NULL) +
-        xlab(.xlab_expression) +
-        ylab(.ylab_expression) +
-        ##  Add the title
-        ggtitle(label = .plot_title) +
-        theme(plot.title = element_text(hjust = 0.5,
-                                        vjust = 0,
-                                        size = 8,
-                                        colour = "brown")) +
-        ##  Add a line for the norm of the global spectrum.
-        geom_hline(yintercept = .the_global_norm,
-                   size = 0.1,
-                   colour = "red",
-                   lty = 1)
-
-    ###  Add a point that shows the 'b'-value used in most of the
-    ###  plots of the local Gaussian spectral densities.
-
-    .plot <-
-        .plot +
-        annotate(geom = "point",
-                 x = 0.5,
-                 y = .df$value[which(.df$bw_points == "0.5")],
-                 size = .7,
-                 shape = 1,
-                 colour = "blue")
-
-    ##  Add information about the value of the global norm.
-    .global_label <- sprintf("D*(f^'%s'*(omega)) == '%s'",
-                             look_up$m_selected,
-                             .the_global_norm)
-    
-    .plot <- .plot +
-        annotate(
-            geom = "text",
-            x = 0,
-            y = .the_global_norm,
-            size = 2,
-            label = .global_label,
-            col = "red",
-            vjust = -0.2,
-            hjust = "inward",
-            parse = TRUE)
-    ##  Update the 'distance_plots'
-    distance_plots[[.name]] <- .plot
 }
+rm(.levels_Diagonal, .levels, input_common)
 
 ###----------------------------------------------------------------###
 
@@ -329,16 +179,12 @@ rm(.range, i, .range_list)
 for (.name in .names)  {
     heatmap_annotations[[.name]]$annotated$size <- 
         .scale * heatmap_annotations[[.name]]$annotated$size
-    distance_annotations[[.name]]$annotated$size <- 
-        .scale * distance_annotations[[.name]]$annotated$size
     #
     heatmap_plots[[.name]] <- 
         heatmap_plots[[.name]] +
         eval(heatmap_annotations[[.name]]$annotated)
-    distance_plots[[.name]] <- 
-        distance_plots[[.name]] +
-        eval(distance_annotations[[.name]]$annotated)
 }
+rm(.scale)
 
 ###----------------------------------------------------------------###
 
@@ -349,7 +195,6 @@ for (.name in .names)  {
     v_just_b <-
         heatmap_annotations[[.name]]$annotated_df["m_value", "vjust"]
     v_just_omega <- - v_just_b
-        ## heatmap_annotations[[.name]]$annotated_df["NC_value", "vjust"]
     heatmap_plots[[.name]] <-
         heatmap_plots[[.name]] +
         annotate(geom = "text",
@@ -375,34 +220,7 @@ for (.name in .names)  {
               axis.ticks.length = unit(.06, "cm"),
               axis.text = element_text(size = 6))
 }
-
-
-###----------------------------------------------------------------###
-
-##  Adjust the placement of omega and b for the axes for the
-##  distance-plot (in order to get the grid-plot better looking).
-
-for (.name in .names)  {
-    size_b <-
-        heatmap_annotations[[.name]]$annotated_df["NC_value", "size"]
-    v_just_b <- 
-        heatmap_annotations[[.name]]$annotated_df["NC_value", "vjust"]
-        ## heatmap_annotations[[.name]]$annotated_df["NC_value", "vjust"]
-    distance_plots[[.name]] <-
-        distance_plots[[.name]] +
-        annotate(geom = "text",
-                 label = "b",
-                 parse = TRUE,
-                 x = Inf,
-                 y = -Inf,
-                 size = 2,
-                 hjust = "inward",
-                 vjust = v_just_b) + 
-        xlab(label = NULL) +
-        theme(axis.ticks = element_line(size = 0.3),
-              axis.ticks.length = unit(.06, "cm"),
-              axis.text = element_text(size = 6))
-}
+rm(.name, .names, v_just_b, v_just_omega, heatmap_annotations)
 
 ###----------------------------------------------------------------###
 
@@ -452,6 +270,7 @@ print(distance_plots$lower,
           layout.pos.col = 1:2))
 
 dev.off()
+rm(.x, .y, .z, .heatmap.pos.row, .distance.pos.row)
 
 ##  Crop the result.  This approach requires that 'pdfcrop' is
 ##  available on the system.
